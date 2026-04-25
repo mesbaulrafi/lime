339a7c50a080c29aae80f2e3c2fee2259b7da768
Mesbaul Haque <mesbaulrafi@gmail.com>
2026-04-26 00:23:55 +0600

    banner Part add

--- src/components/layouts/Benner.jsx
+++ src/components/layouts/Benner.jsx
@@ -1,27 +1,27 @@
-// import React from "react";
-// import Images from "../Images";
-// import BennerImg from "/src/assets/bennerImg.png";
-// import Conteinar from "../Conteinar";
-// import { Link } from "react-router-dom";
-// import Button from "../Button";
-// import Flex from "../Flex";
+import React from "react";
+import Images from "../Images";
+import BennerImg from "/src/assets/bennerImg.png";
+import Conteinar from "../Conteinar";
+import { Link } from "react-router-dom";
+import Button from "../Button";
+import Flex from "../Flex";
 
-// const Benner = () => {
-//   return (
-//     <div className="">
-//       <div className="bg-[url('/src/assets/bennerImg.png')] pt-[560px] pb-[350px] bg-cover bg-center bg-no-repeat">
-//       <Conteinar>
-//           <h2 className="text-white text-[72px] text-center">Ride Green</h2>
-//         <div className={" text-center"}>
-//           <Link to={'/'} className="px-2.5"><Button btnText={"Our cities"}/></Link>
-//           <Link to={'/'} className=""><Button btnText={"Download the App"}/></Link>
-//         </div>
-//       </Conteinar>
-//       </div>
+const Benner = () => {
+  return (
+    <div className="">
+      <div className="bg-[url('/src/assets/bennerImg.png')] pt-[560px] pb-[350px] bg-cover bg-center bg-no-repeat">
+      <Conteinar>
+          <h2 className="text-white text-[72px] text-center">Ride Green</h2>
+        <div className={" text-center"}>
+          <Link to={'/'} className="px-2.5"><Button btnText={"Our cities"}/></Link>
+          <Link to={'/'} className=""><Button btnText={"Download the App"}/></Link>
+        </div>
+      </Conteinar>
+      </div>
 
-//     </div>
-//   );
-// };
+    </div>
+  );
+};
 
-// export default Benner;
+export default Benner;
 
