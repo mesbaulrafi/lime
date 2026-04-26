62fca6bb919a0f3dd7862fb3fb7d4ba7f0109f34
Mesbaul Haque <mesbaulrafi@gmail.com>
2026-04-27 01:05:55 +0600

    add location part

---
+++ src/components/layouts/Locations.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+const Locations = () => {
+  return (
+    <>
+      <section>
+        <div className="">
+          <container>
+            <div className="">
+              <div className="text-center pb-18 ">
+                <h5 className="text-[#00B200] text-xl ">The App</h5>
+                <h3 className="text-[40px] ">How to Lime</h3>
+              </div>
+            </div>
+          </container>
+        </div>
+      </section>
+    </>
+  );
+};
+
+export default Locations;
