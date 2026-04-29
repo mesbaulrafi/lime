a76e483513484a203067d72d5e27c77bb95048dd
Mesbaul Haque <mesbaulrafi@gmail.com>
2026-04-30 00:54:42 +0600

    add : Blog Part

---
+++ src/components/layouts/Blog.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import Conteinar from "../Conteinar";
+
+const Blog = () => {
+  return (
+    <>
+      <section>
+        <div className="">
+          <Conteinar>
+            <div className=""></div>
+          </Conteinar>
+        </div>
+      </section>
+    </>
+  );
+};
+
+export default Blog;
